16ce178aeaa31595247b7cbd1c6a18e186a08066
umang00arora <umangarora810@gmail.com>
2024-03-27 20:25:33 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,177 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useContext, useEffect, useState } from "react";
+import Card from "./components/Card";
+import CreatePlaylist from "./components/CreatePlaylist";
+import { initializePlaylist } from "./initialize";
+import Navbar from "./components/Navbar";
+import { MusicContext } from "./Context";
 
 function App() {
+  const [keyword, setKeyword] = useState("");
+  const [message, setMessage] = useState("");
+  const [tracks, setTracks] = useState([]);
+  const [token, setToken] = useState(null);
+
+  const musicContext = useContext(MusicContext);
+  const isLoading = musicContext.isLoading;
+  const setIsLoading = musicContext.setIsLoading;
+  const setLikedMusic = musicContext.setLikedMusic;
+  const setpinnedMusic = musicContext.setPinnedMusic;
+  const resultOffset = musicContext.resultOffset;
+  const setResultOffset = musicContext.setResultOffset;
+
+  const fetchMusicData = async () => {
+    setTracks([]);
+    window.scrollTo(0, 0);
+    setIsLoading(true);
+    try {
+      const response = await fetch(
+        `https://api.spotify.com/v1/search?q=${keyword}&type=track&offset=${resultOffset}`,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error("Failed to fetch music data");
+      }
+
+      const jsonData = await response.json();
+
+      setTracks(jsonData.tracks.items);
+    } catch (error) {
+      setMessage(error.message);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  const handleKeyPress = (event) => {
+    if (event.key === "Enter") {
+      setResultOffset(0);
+      fetchMusicData();
+    }
+  };
+
+  useEffect(() => {
+    initializePlaylist();
+
+    // current client credentials will be deleted in few days
+    const fetchToken = async () => {
+      try {
+        const response = await fetch("https://accounts.spotify.com/api/token", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/x-www-form-urlencoded",
+          },
+          body: "grant_type=client_credentials&client_id=a94b5327623b4c2cb2a6a1d1db806c56&client_secret=fb66df0060d54569912c5c58648f4ae2",
+        });
+
+        if (!response.ok) {
+          throw new Error("Failed to fetch token");
+        }
+
+        const jsonData = await response.json();
+        setToken(jsonData.access_token);
+      } catch (error) {
+        setMessage(error.message);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    fetchToken();
+    setLikedMusic(JSON.parse(localStorage.getItem("likedMusic")));
+    setpinnedMusic(JSON.parse(localStorage.getItem("pinnedMusic")));
+  }, [setIsLoading, setLikedMusic, setpinnedMusic]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar
+        keyword={keyword}
+        setKeyword={setKeyword}
+        handleKeyPress={handleKeyPress}
+        fetchMusicData={fetchMusicData}
+      />
+
+      <div className="container">
+        <div className={`row ${isLoading ? "" : "d-none"}`}>
+          <div className="col-12 py-5 text-center">
+            <div
+              className="spinner-border"
+              style={{ width: "3rem", height: "3rem" }}
+              role="status"
+            >
+              <span className="visually-hidden">Loading...</span>
+            </div>
+          </div>
+        </div>
+        <div className="row">
+          {tracks.map((element) => {
+            return <Card key={element.id} element={element} />;
+          })}
+        </div>
+        <div className="row" hidden={tracks.length === 0}>
+          <div className="col">
+            <button
+              onClick={() => {
+                setResultOffset((previous) => previous - 20);
+                fetchMusicData();
+              }}
+              className="btn btn-outline-success w-100"
+              disabled={resultOffset === 0}
+            >
+              Previous Next Page: {resultOffset / 20}
+            </button>
+          </div>
+          <div className="col">
+            <button
+              onClick={() => {
+                setResultOffset((previous) => previous + 20);
+                fetchMusicData();
+              }}
+              className="btn btn-outline-success w-100"
+            >
+              Next Page: {resultOffset / 20 + 2}
+            </button>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col">
+            <h4 className="text-center text-danger py-2">{message}</h4>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-12 py-5 text-center">
+            <h1>
+              <i className="bi bi-music-note-list mx-3"></i>
+              ARORA MUSIC PLAYER
+            </h1>
+            <h3 className="py-5">Discover music in 30 seconds</h3>
+            <div>
+              <a
+                target="_blank"
+                rel="noreferrer"
+                className="btn btn-outline-dark"
+                href="https://github.com/umang00arora"
+              >
+                <i className="bi bi-github mx-2"></i>Github
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div
+        className="modal fade position-absolute"
+        id="exampleModal"
+        tabIndex={-1}
+        aria-labelledby="exampleModalLabel"
+        aria-hidden="true"
+      >
+        <CreatePlaylist />
+      </div>
+    </>
   );
 }
 
